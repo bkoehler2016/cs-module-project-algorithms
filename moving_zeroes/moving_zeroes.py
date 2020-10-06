19f321dea609284477cf860101e233f8be50427c
Benjamin Koehler <bkoehler751@gmail.com>
2020-10-06 07:52:46 -0700

    fixed issue with product of other numbers

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -4,12 +4,14 @@ Returns: a List of integers
 '''
 def moving_zeroes(arr):
     # Your code here
+    #set a count
     count = 0
+    #for loop if not 0 set count to the index increase by 1
     for i in range(len(arr)):
         if(arr[i] !=0):
             arr[count] = arr[i]
             count += 1
-
+# while count < len(arr) set to 0 and incresee by one
     while count < len(arr):
         arr[count] = 0
         count += 1
