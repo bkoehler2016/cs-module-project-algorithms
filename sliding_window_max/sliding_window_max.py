4256a61255ccc5a1666fce47f5b1f2ee05753a6e
Benjamin Koehler <bkoehler751@gmail.com>
2020-10-06 11:15:21 -0700

    completed sliding window max takes 12.99 seconds

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -4,8 +4,17 @@ Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
     # Your code here
-
-    pass
+    # need an empty array to hold new values based on k value
+    max_size = []
+    # need an index so we can go through and set the highest number to it
+    index = 0
+    # while loop to append values to new array
+    while index + k <= len(nums):
+        # find the max number in sliced array
+        max_size.append(max(nums[index:(k + index)]))
+        #increase index by one so we can move the slider
+        index += 1
+    return max_size
 
 
 if __name__ == '__main__':
